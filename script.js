7ac98be73cadc2184f9feead7a9801349d79a76e
sderr01 <sarah1ujp@gmail.com>
2025-01-08 18:32:14 -0500

    implemented user input with grid size + fixed some of the coloring issues

--- script.js
+++ script.js
@@ -1,38 +1,55 @@
-// const para = document.createElement('p');
+// container for canvas
 const container = document.querySelector('.container');
-// para.textContent = "HEllo";
-// container.appendChild(para);
-// para.style.color = 'red';
 
-// const node = document.createElement('div');
-// node.setAttribute('style', 'width: 100px; height: 100px; background: blue;');
-// container.appendChild(node);
-// const node2 = document.createElement('div');
-// node2.setAttribute('style', 'width: 100px; height: 100px; background: red;');
-// container.appendChild(node2);
+// user input for grid size
+let answer;
 
-
-// node.addEventListener('mouseover', function(e) {
-//     e.target.style.background = 'pink';
-// });
-
-
-for(let j=0; j<16; j++){
-    const col = document.createElement('div');
-    
-    // adding new node per col
-    for (let i = 0; i < 16; i++){
-        const node = document.createElement('div');
-        node.setAttribute('style', 'width: 50px; height: 50px; background: blue');
-        node.style.margin = '1px';
-        col.appendChild(node);
+// creates grid based on either default 16x16 or with user input
+function gridCreation(answer){
+    // base
+    if (answer == 0 || answer == undefined){
+        answer = 16;
     }
 
-    //adding column to container
-    container.appendChild(col);
+    for(let j = 0; j < answer; j++){
+        const col = document.createElement('div');
+        
+        // adding new node per col
+
+        // default: 816x816px, cell is 50x50px with 1px margin
+        for (let i = 0; i < answer; i++){
+            const node = document.createElement('div');
+            node.setAttribute('style', 'width: 50px; height: 50px; background: black');
+            node.style.margin = '1px';
+            col.appendChild(node);
+    
+            //  color change 
+            node.addEventListener('mouseover', function(e){
+                e.target.style.background = 'white';
+            });
+        }
+    
+        //adding column to container
+        container.appendChild(col);
+    }    
 }
 
+// clear previous canvas before creating new one
+// sets all container elements to ""
+function gridClear(){
+    container.innerHTML = "";
+}
 
-container.addEventListener('mouseover', function(e){
-    e.target.style.background = 'pink';
+// button for grid size
+const gridsize = document.getElementById("gridsize");
+
+// clears grid then creates new grid with user input
+gridsize.addEventListener("click", function (e){
+    answer = prompt("grid");
+    gridClear();
+    gridCreation(answer);
 });
+
+
+gridCreation();
+
